85b24972be5f0f8b1a32af8b1439a4eda2f10de0
warriorstamps <warriorstamps.rocks.1@gmail.com>
2024-10-23 21:43:27 -0500

    try adding main class

--- Main.java
+++ Main.java
@@ -4,53 +4,80 @@ import java.io.*;
 
 public class Main
 {
-    public static void main(String [] args)
+    public static <T> void main(String [] args)
     {
         Scanner scnr = new Scanner(System.in);
 
         System.out.print("Enter the database file: "); //for database file
-        scnr.next();
+        String databaseName = scnr.next();
+        scnr = new Scanner(new File(databaseName));
         //open database file
         try
         {
             System.out.print("Enter the batch file"); //for command file
-            scnr.next();
-    
+            String batchName = scnr.next();
+            scnr = new Scanner(new File(batchName));
             //open batch file
             try
             {
                 while(scnr.hasNextLine())
                 {
-                    int num = scnr.subString(0, 2); //get first number 
-                    if(num == 1) //add a record
+                    String line = scnr.nextLine();
+                    String num = line.substring(0, 2); //get first number 
+                    if(num == "1") //add a record
+                    {
+                        T payload = (T)line.substring(3);
+                        insert(payload);
+                    }
+                    else if(num == "2") //search for a record
+                    {
+                        String searchTerm = line.substring(3);
+                        Node<T> check = search(searchTerm);
+                        if(check == null) //null
+                        {
+                            System.out.println(searchTerm + " NOT FOUND"); //not found message 
+                        }
+                        else
+                        {
+                            System.out.println(searchTerm + " FOUND"); //found message 
+                            //print contents of game
+                            System.out.println("Name: " + node.getName());
+                            System.out.println("Highscore: " + node.getHighScore());
+                            System.out.println("Initals: " + node.getInitals());
+                            System.out.println( "Plays: " + node.getPlays());
+                            System.out.println( "Revenue: " + node.getRevenue());
+                        }
+                    }
+                    /* 
+                    else if(num == "3") //edit a record
                     {
     
                     }
-                    else if(num == 2) //search for a record
+                    else if(num == "4") //delete a record
                     {
     
                     }
-                    else if(num == 3) //edit a record
-                    {
-    
-                    }
-                    else if(num == 4) //delete a record
-                    {
-    
-                    }
-                    else if(num == 5) //sort the records (accending alphabeticly), implement later
+                    else if(num == "5") //sort the records (accending alphabeticly), implement later
                     {
                         
                     }
+                    */
                     else //invalid input
                     {
                         scnr.nextLine();
                     }
                 }
+                //write to ciderCade.dat
+
+                //close database and batch files
+                batchName.close();
+                databaseName.close();
             }
             catch (FileNotFoundException e)
             {
                 System.out.print("Batch file not found");
+                //close database file
+                databaseName.close();
             }
         }
         catch (FileNotFoundException e)
